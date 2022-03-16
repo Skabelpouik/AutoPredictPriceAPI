e66b4b3c68e1fd265c8b2a6a43259abbce841ec0
FannyS <fanny.scheltgen@gmail.com>
2022-03-16 16:52:21 +0100

    Test import model avec joblib et pipeline

--- predict_app/app.py
+++ predict_app/app.py
@@ -3,6 +3,7 @@ from flask_restful import Api, Resource
 from sklearn import tree
 import numpy as np
 import pickle as p
+from joblib import load
 
 app = Flask(__name__)
 
@@ -11,12 +12,14 @@ api = Api(app)
 class Home(Resource):
     def get(self):
         j_data = request.get_json()
-        prediction = np.array2string(model.predict(j_data))
+        #prediction = np.array2string(model.predict(j_data))
+        prediction = np.array2string(pipeline.predict(j_data))
         return jsonify(prediction)
 
 api.add_resource(Home, '/')
 
 if __name__ == '__main__':
-    modelfile = 'predict_app/models/final_prediction.pickle'    
-    model = p.load(open(modelfile, 'rb'))
+    #modelfile = 'predict_app/models/final_prediction.pickle'    
+    #model = p.load(open(modelfile, 'rb'))
+    pipeline = load("predict_app/models/text_classification.joblib")
     app.run(debug=True)
